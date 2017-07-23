b3d06915503b1ec7a686fddccfa7238ba459b52e
CarlOlsson <carlolsson.co@gmail.com>
2017-07-23 16:39:16 +0200

    display all datapoints on the x axis

--- gui_functions/update_plot.m
+++ gui_functions/update_plot.m
@@ -1,13 +1,21 @@
 function update_plot( handles )
+x_min = handles.data.(handles.currently_displayed_variables(1).topic).timestamp(1)./1e6;
+x_max = handles.data.(handles.currently_displayed_variables(1).topic).timestamp(end)./1e6;
 for i = 1:length(handles.currently_displayed_variables)
     time_vector = handles.data.(handles.currently_displayed_variables(i).topic).timestamp./1e6; % convert from us to s
+    if time_vector(1) < x_min
+        x_min = time_vector(1);
+    end
+    if time_vector(end) > x_max
+        x_max = time_vector(end);
+    end
     h = plot(time_vector, handles.data.(handles.currently_displayed_variables(i).topic).(handles.currently_displayed_variables(i).field),handles.line_spec,'linewidth',handles.line_width, 'Color', handles.currently_displayed_variables(i).color);
     hold on
 end
 
 hold off
 grid on
-xlim([time_vector(1) time_vector(end)])
+xlim([x_min x_max])
 legend({handles.currently_displayed_variables.full_name}, 'Interpreter', 'none');
 xlabel('Time [s]')
 
